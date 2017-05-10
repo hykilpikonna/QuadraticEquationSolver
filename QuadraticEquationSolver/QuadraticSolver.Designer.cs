d2cd5ca229c9f58b35b10abad901c1a0dc2f5789
kilpikonna <871674895@qq.com>
2017-05-10 09:46:17 -0400

    First commit

---
+++ QuadraticEquationSolver/QuadraticSolver.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace QuadraticEquationSolver
+{
+    partial class QuadraticSolver
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.Calculate = new System.Windows.Forms.Button();
+            this.Output1 = new System.Windows.Forms.TextBox();
+            this.Output2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(12, 25);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(260, 20);
+            this.Input.TabIndex = 0;
+            this.Input.Text = "ax^2+bx+c";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Input";
+            // 
+            // Calculate
+            // 
+            this.Calculate.Location = new System.Drawing.Point(12, 51);
+            this.Calculate.Name = "Calculate";
+            this.Calculate.Size = new System.Drawing.Size(260, 23);
+            this.Calculate.TabIndex = 3;
+            this.Calculate.Text = "Calculate";
+            this.Calculate.UseVisualStyleBackColor = true;
+            this.Calculate.Click += new System.EventHandler(this.Calculate_Click);
+            // 
+            // Output1
+            // 
+            this.Output1.Location = new System.Drawing.Point(12, 103);
+            this.Output1.Name = "Output1";
+            this.Output1.ReadOnly = true;
+            this.Output1.Size = new System.Drawing.Size(260, 20);
+            this.Output1.TabIndex = 4;
+            // 
+            // Output2
+            // 
+            this.Output2.Location = new System.Drawing.Point(12, 151);
+            this.Output2.Name = "Output2";
+            this.Output2.ReadOnly = true;
+            this.Output2.Size = new System.Drawing.Size(260, 20);
+            this.Output2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Output1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Output2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 186);
+            this.Controls.Add(this.Output2);
+            this.Controls.Add(this.Output1);
+            this.Controls.Add(this.Calculate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Calculate;
+        private System.Windows.Forms.TextBox Output1;
+        private System.Windows.Forms.TextBox Output2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ QuadraticEquationSolver/QuadraticSolver.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuadraticEquationSolver
+{
+    public partial class QuadraticSolver : Form
+    {
+        public QuadraticSolver()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate_Click(object sender, EventArgs e)
+        {
+            String tempEquation = Input.Text;
+            ArrayList equation = new ArrayList();
+            String tempString = "";
+            for (int i = 0; i < tempEquation.Length; i++)
+            {
+                if (!(tempEquation[i].Equals("+")))
+                {
+                    tempString += tempEquation[i];
+                }
+                else
+                {
+                    equation.Add(tempString);
+                    tempString = "";
+                }
+            }
+            if (equation.Count == 3)
+            {
+                int a = -600, b = -600, c = -600;
+
+                tempString = "";
+                for (int i = 0; i < 3; i++)
+                {
+                    for (int j = 0; j < ((String)equation[i]).Length; j++)
+                    {
+                        if (((String)equation[i])[j].Equals('-'))
+                        {
+                            
+                        }
+                        else if (((String)equation[i])[j].Equals('x'))
+                        {
+                            switch(i)
+                            {
+                                case 0:
+                                    a = Int32.Parse(tempString);
+                                    break;
+                                case 1:
+                                    b = Int32.Parse(tempString);
+                                    break;
+                                case 2:
+                                    c = Int32.Parse(tempString);
+                                    break;
+                                default:
+                                    break;
+                            }
+                            
+                        }
+                        else
+                        {
+                            tempString += ((String)equation[i])[j];
+                        }
+                    }
+                }
+                 
+                if (((String)equation[0])[0].Equals('-'))
+                {
+                    a = a * -1;
+                }
+                if (((String)equation[1])[0].Equals('-'))
+                {
+                    b = b * -1;
+                }
+                if (((String)equation[2])[0].Equals('-'))
+                {
+                    c = c * -1;
+                }
+
+                Output1.Text = (((0 - b) + Math.Sqrt((b ^ 2) - (4 * a * c))) / 2).ToString();
+                Output2.Text = (((0 - b) - Math.Sqrt((b ^ 2) - (4 * a * c))) / 2).ToString();
+            }
+            else
+            {
+                Output1.Text = "Error.(arraylist length != 3)";
+                Output2.Text = "arraylist.length = " + equation.Count;
+            }
+        }
+    }
+}
